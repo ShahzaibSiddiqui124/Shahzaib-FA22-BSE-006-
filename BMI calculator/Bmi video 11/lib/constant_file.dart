6122b0ef1a81608aa3048b527a6c27b25e7fbf0d
ShahzaibSiddiqui124 <shahzaibsiddiqui.cui@gmail.com>
2025-05-04 01:36:09 +0500

    video 11
    use of slider in app

---
+++ BMI calculator/Bmi video 11/lib/constant_file.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const kLabelStyle = TextStyle(
+  fontSize: 30.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+  color: Color(0xFF8D8E98),
+);
+
+const double kMinHeight = 120.0;
+const double kMaxHeight = 220.0;
